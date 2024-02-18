bc89e05752bb4d385d2a671cac8127c04d0c6ea9
Ryan Gaudon <ryan@gaudon.ca>
2024-02-18 02:23:07 -0330

    Sending raw strings

--- services/uart_service.py
+++ services/uart_service.py
@@ -61,15 +61,13 @@ class UartService(BaseService):
 
     async def transmit_heart_rate_data(self):
         if self.message is not None:
-            self.uart.write(bytes(self.message + '\n', 'utf-8'))
+            self.uart.write(self.message + '\n')
             self.message = None
     
     
     async def receive_heart_rate_data(self):
         while self.uart.any() > 0:
             self.data_rec = self.uart.readline()
-            if self.data_rec is not None:
-                self.data_rec = self.data_rec.strip('\n')
         
         if self.data_rec is not None:
             print("[UartService] : Data Received - {0}".format(self.data_rec))
